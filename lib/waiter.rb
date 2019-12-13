b0f9db8090f6991f10898d4f96f16fe10a4b700b
Sean Beach <sean.beach1@yahoo.com>
2019-12-13 17:34:06 -0500

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class Waiter
+    attr_reader :name, :years_experience
+
+    @@all = []
+
+    def initialize(name, years_experience)
+        @name, @years_experience = name, years_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip=0)
+        Meal.new(self, customer, total, tip)
+    end
+    
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+    
+    def best_tipper
+        best_tipped_meal = self.meals.max do |meal_a, meal_b|
+        meal_a.tip <=> meal_b.tip
+        end
+        best_tipped_meal.customer
+    end
 
 end
